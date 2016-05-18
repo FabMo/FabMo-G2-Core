60057c7753894739323488d76bc5a2b5471d934a
Ryan Sturmer <ryasnturmer@gmail.com>
2016-05-17 21:36:17 -0400

    Fix bug in flags

--- TinyG2/config.h
+++ TinyG2/config.h
@@ -220,7 +220,7 @@ typedef enum {						    // value typing for config and JSON
 #define F_PERSIST 		0x02			// persist this item when set is run
 #define F_NOSTRIP		0x04			// do not strip the group prefix from the token
 #define F_CONVERT		0x08			// set if unit conversion is required
-#define F_ICONVERT		0x01            // set if unit conversion is required AND value is an inverse quantity
+#define F_ICONVERT		0x10            // set if unit conversion is required AND value is an inverse quantity
 
 #define _f0				0x00
 #define _fi				(F_INITIALIZE)
