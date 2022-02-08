3b31ec36d7cc93bd48a580aef1f740d9e88bbcf7
Matt Staniszewski <matt@msxconsulting.com>
2022-02-08 13:38:07 -0800

    Improved fix for #27, covers all axes including rotary (ABC) and UVW

--- g2core/canonical_machine.cpp
+++ g2core/canonical_machine.cpp
@@ -1220,11 +1220,13 @@ stat_t cm_resume_g92_offsets()
 
 void cm_axes_to_mm(const float *target_global, float *target_mm, const bool *flags) // Assumes both target arrays are the same size
 {
-    for (uint8_t axis = AXIS_X; axis <= AXIS_C; axis++) {               // Only convert linears (not rotaries)
+    for (uint8_t axis = AXIS_X; axis < AXES; axis++) {
         if (!flags[axis] || cm->a[axis].axis_mode == AXIS_DISABLED) {
             continue;                                                   // skip axis if not flagged for update or its disabled
+        } else if (axis > LAST_LINEAR_AXIS) {
+            target_mm[axis] = target_global[axis];                      // pass through rotary axes (no unit conversion required)
         } else {
-            target_mm[axis] = _to_millimeters(target_global[axis]);
+            target_mm[axis] = _to_millimeters(target_global[axis]);     // convert linear axes
         }
     }
 }
