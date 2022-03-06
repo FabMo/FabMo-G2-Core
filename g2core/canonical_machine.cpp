d28800f581033298cc12b0d74a15f9819e271964
Cosmic7373 <brass@pdx.edu>
2022-03-06 06:12:03 -0800

    Pushing cleaned up final fix for #34
    This is the cleaned up and hopefully final fix for issue #34

--- g2core/canonical_machine.cpp
+++ g2core/canonical_machine.cpp
@@ -577,9 +577,6 @@ float cm_get_display_position(const GCodeState_t *gcode_state, const uint8_t axi
         position = mp_get_runtime_display_position(axis);
     }
     if (axis <= LAST_LINEAR_AXIS) {   // linears
-        //if (gcode_state->units_mode == INCHES) {
-            //position /= MM_PER_INCH;
-        //}
         position = _to_inches(position);
     }
     return (position);
